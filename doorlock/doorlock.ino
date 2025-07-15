707e3ceac7c2a9f63829858811eea34886710567
alsgudrl132 <alsgudrl132@gmail.com>
2025-07-15 12:43:22 +0900

    부저 추가

--- doorlock/doorlock.ino
+++ doorlock/doorlock.ino
@@ -1,10 +1,11 @@
-
 #include <LiquidCrystal.h>
 
 const int rs = 13, en = 12, d4 = 11, d5 = 10, d6 = 9, d7 = 8;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 const int rowPins[4] = {A0, A1, A2, A3};  // R1~R4
 const int colPins[4] = {A4, A5, 2, 3};    // C1~C4
+const int buzzerPin = 4;
+int count = 0;
 
 char keymap[4][4] = {
   { '1', '2', '3', 0   },
@@ -14,6 +15,7 @@ char keymap[4][4] = {
 };
 
 void setup() {
+  pinMode(buzzerPin, OUTPUT);
   lcd.begin(16, 2);
   lcd.print("press, password");
   Serial.begin(9600);
@@ -25,17 +27,43 @@ void setup() {
 }
 
 void loop() {
+  bool keyPressed = false;
+
   for (int c = 0; c < 4; c++) {
     digitalWrite(colPins[c], LOW);
     for (int r = 0; r < 4; r++) {
       if (digitalRead(rowPins[r]) == LOW) {
         char key = keymap[r][c];
+        if (key >= '0' && key <= '9') {
+          if(count <= 4) {
+            Serial.print("count =");
+            Serial.println(count);
+            Serial.println(key);
+            tone(buzzerPin, 262);
+            delay(200);
+            noTone(buzzerPin);
+            switch (count) {
+              case 0 : lcd.setCursor(0, 1); lcd.print("    _ _ _ _"); break;
+              case 1 : lcd.setCursor(0, 1); lcd.print("    * _ _ _"); break;
+              case 2 : lcd.setCursor(0, 1); lcd.print("    * * _ _"); break;
+              case 3 : lcd.setCursor(0, 1); lcd.print("    * * * _"); break;
+              case 4 : lcd.setCursor(0, 1); lcd.print("    * * * *"); break;
+            }
+            keyPressed = true;
+            delay(100);  // 디바운스
+            count++;
+          }
+          else {
+            count = 0;
+          }
+          
+        }
       }
-      lcd.setCursor(0, 1);
-      lcd.print("    _ _ _ _");
-      delay(200);  // 디바운스
     }
     digitalWrite(colPins[c], HIGH);
   }
-}
 
+  if (!keyPressed) {
+    noTone(buzzerPin);
+  }
+}
