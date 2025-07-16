2029bf8572d59f6daa03d3635035588372699045
alsgudrl132 <alsgudrl132@gmail.com>
2025-07-16 11:56:06 +0900

    패스워드설정, 패스워드에 따른 메세지 출력

--- doorlock/doorlock.ino
+++ doorlock/doorlock.ino
@@ -1,29 +1,70 @@
-#include <LiquidCrystal.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
 
-const int rs = 13, en = 12, d4 = 11, d5 = 10, d6 = 9, d7 = 8;
-LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
-const int rowPins[4] = {A0, A1, A2, A3};  // R1~R4
-const int colPins[4] = {A4, A5, 2, 3};    // C1~C4
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+const int rowPins[4] = {A0, A1, A2, A3};     // 키패드 행
+const int colPins[4] = {6, 7, 2, 3};         // 키패드 열
 const int buzzerPin = 4;
-int count = 0;
 
-char keymap[4][4] = {
-  { '1', '2', '3', 0   },
-  { '4', '5', '6', 0   },
-  { '7', '8', '9', 0   },
-  {  0 , '0',  0 , 0   }
+int accessKey[4] = {1, 5, 4, 1}; // 정답 비밀번호 (예)
+int typeKey[4];                  // 입력 저장용
+int index = 0;                   // 입력 인덱스
+bool isAccess = false;
+byte keymap[4][4] = {
+  { 1,  2,  3,  0  },
+  { 4,  5,  6,  0  },
+  { 7,  8,  9,  0  },
+  { 0,  0,  0, 10 }
 };
+
+void addKey(byte key) {
+  if (index < 4 && key <= 9) { // 숫자만 저장
+    typeKey[index++] = key;
+  }
+}
 
 void setup() {
   pinMode(buzzerPin, OUTPUT);
-  lcd.begin(16, 2);
-  lcd.print("press, password");
+  lcd.init();
+  lcd.backlight();
+  lcd.setCursor(0, 0);
+  lcd.print("press password");
   Serial.begin(9600);
+
   for (int i = 0; i < 4; i++) {
     pinMode(rowPins[i], INPUT_PULLUP);
     pinMode(colPins[i], OUTPUT);
     digitalWrite(colPins[i], HIGH);
   }
+}
+
+void resetInput() {
+  index = 0;
+  lcd.setCursor(0, 1);
+  lcd.print("    _ _ _ _    ");
+  noTone(buzzerPin);
+}
+
+void printAsterisks() {
+  lcd.setCursor(0, 1);
+  for (int i = 0; i < 4; i++) {
+    if(i == 0) {
+      if (i < index) lcd.print("    * ");
+      else lcd.print("    _ ");
+    }
+    else {
+      if (i < index) lcd.print("* ");
+      else lcd.print("_ ");
+    }
+    
+  }
+}
+
+bool checkPassword() {
+  for (int i = 0; i < 4; i++) {
+    if (typeKey[i] != accessKey[i]) return false;
+  }
+  return true;
 }
 
 void loop() {
@@ -33,30 +74,52 @@ void loop() {
     digitalWrite(colPins[c], LOW);
     for (int r = 0; r < 4; r++) {
       if (digitalRead(rowPins[r]) == LOW) {
-        char key = keymap[r][c];
-        if (key >= '0' && key <= '9') {
-          if(count <= 4) {
-            Serial.print("count =");
-            Serial.println(count);
-            Serial.println(key);
+        byte key = keymap[r][c];
+        if (key <= 9) {
+          if (index < 4) {
             tone(buzzerPin, 262);
             delay(200);
             noTone(buzzerPin);
-            switch (count) {
-              case 0 : lcd.setCursor(0, 1); lcd.print("    _ _ _ _"); break;
-              case 1 : lcd.setCursor(0, 1); lcd.print("    * _ _ _"); break;
-              case 2 : lcd.setCursor(0, 1); lcd.print("    * * _ _"); break;
-              case 3 : lcd.setCursor(0, 1); lcd.print("    * * * _"); break;
-              case 4 : lcd.setCursor(0, 1); lcd.print("    * * * *"); break;
-            }
+
+            addKey(key);
+            printAsterisks();
+
             keyPressed = true;
-            delay(100);  // 디바운스
-            count++;
+            Serial.println(key);
+
+            delay(150);  // 디바운스
           }
-          else {
-            count = 0;
+        } 
+        else if (key == 10) {  // 확인 버튼
+          tone(buzzerPin, 500);
+          delay(200);
+          noTone(buzzerPin);
+
+          if (index == 4) {
+            Serial.println(typeKey[0]);
+            Serial.println(typeKey[1]);
+            Serial.println(typeKey[2]);
+            Serial.println(typeKey[3]);
+            if (checkPassword()) {
+              lcd.setCursor(0, 1);
+              lcd.print("Correct!      ");
+              Serial.println("Password correct");
+              isAccess = true;
+            } else {
+              lcd.setCursor(0, 1);
+              lcd.print("Wrong!        ");
+              Serial.println("Password wrong");
+              isAccess = false;
+            }
+          } else {
+            lcd.setCursor(0, 1);
+            lcd.print("Incomplete!   ");
+            Serial.println("Password incomplete");
           }
-          
+
+          delay(1000);
+          resetInput();
+          keyPressed = true;
         }
       }
     }
